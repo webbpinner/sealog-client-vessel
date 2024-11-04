c6785c65b10f6d86950184f83d9006b3d7cc63c8
Webb Pinner <webb.pinner@oceanxplorer.org>
2024-11-04 08:53:20 +0800

    fix bug preventing cruise file uploads

--- src/actions/index.js
+++ src/actions/index.js
@@ -715,7 +715,7 @@ export const updateCruise = (formProps) => {
   let fields = { ...formProps }
   delete fields.id
   delete fields.cruise_access_list
-  delete fields.cruise_additional_meta.cruise_files
+  // delete fields.cruise_additional_meta.cruise_files
 
   return async (dispatch) => {
     const response = await update_cruise(fields, formProps.id)
